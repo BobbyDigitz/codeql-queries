4ef36527409e689f7f2e0cb7c36d666dc7bfdee8
BobbyDigitz <76012943+BobbyDigitz@users.noreply.github.com>
2024-11-22 16:38:28 -0800

    Update Test.java

--- tailor-packs/java-poc/unit-tests/local_is_remote/Test.java
+++ tailor-packs/java-poc/unit-tests/local_is_remote/Test.java
@@ -1,17 +1,8 @@
-import java.io.*;
-import java.net.InetAddress;
-import java.nio.file.Path;
-import java.nio.file.FileSystems;
-
-class Test {
-	void doGet1(InetAddress address)
-		throws IOException {
-			String temp = address.getHostName();
+ess.getHostName();
 			
 			// BAD: construct a file path with user input
 			File file = new File(temp);
 
-			// MAYBE BAD: construct a path with local user input
-			Path path = FileSystems.getDefault().getPath(System.getenv("PATH"));
+			// MAYBE BAD: construct a path with tem.getenv("PATH"));
 	}
 }
