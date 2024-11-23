db662e0a0107bd85ff6aaf3df4ee6d5cab0234c6
BobbyDigitz <76012943+BobbyDigitz@users.noreply.github.com>
2024-11-22 16:36:47 -0800

    Update Test.java

--- tailor-packs/java-poc/unit-tests/taint_through_crudrepository_save/Test.java
+++ tailor-packs/java-poc/unit-tests/taint_through_crudrepository_save/Test.java
@@ -3,15 +3,9 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import org.springframework.data.repository.CrudRepository;
+epository<String, String> cr = null;
 
-public class Test extends HttpServlet {
-	public static CrudRepository<String, String> cr = null;
-
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-	throws ServletException, IOException {
-		String taint = request.getParameter("page");
-		taint = cr.save(taint);
+;
 		response.getWriter().print("a " + taint);
 	}
 }
